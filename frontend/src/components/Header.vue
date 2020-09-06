7fe84c0db3431a7428c9db24c95d091271df9194
Shihab Hasan <shihabhasan.iut@gmail.com>
2020-09-06 19:07:15 +0600

    changes for klovercloud (share button & global settings hidden)

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -20,7 +20,7 @@
         <!-- Menu that shows on listing AND mobile when there are files selected -->
         <div id="file-selection" v-if="isMobile && isListing">
           <span v-if="selectedCount > 0">{{ selectedCount }} selected</span>
-          <share-button v-show="showShareButton"></share-button>
+          <!--<share-button v-show="showShareButton"></share-button>-->
           <rename-button v-show="showRenameButton"></rename-button>
           <copy-button v-show="showCopyButton"></copy-button>
           <move-button v-show="showMoveButton"></move-button>
@@ -30,7 +30,7 @@
         <!-- This buttons are shown on a dropdown on mobile phones -->
         <div id="dropdown" :class="{ active: showMore }">
           <div v-if="!isListing || !isMobile">
-            <share-button v-show="showShareButton"></share-button>
+            <!--<share-button v-show="showShareButton"></share-button>-->
             <rename-button v-show="showRenameButton"></rename-button>
             <copy-button v-show="showCopyButton"></copy-button>
             <move-button v-show="showMoveButton"></move-button>
